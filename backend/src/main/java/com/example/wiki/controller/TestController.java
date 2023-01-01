54a130f46055119b037ffd412bf1d5b6e16b86d9
lry6789 <cleansexd1@gmail.com>
2023-01-01 17:08:13 -0600

    helloworld api test

--- backend/src/main/java/com/example/wiki/controller/TestController.java
+++ backend/src/main/java/com/example/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.wiki.controller;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,9 @@ public class TestController {
     @RequestMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world! Post."+name ;
     }
 }
