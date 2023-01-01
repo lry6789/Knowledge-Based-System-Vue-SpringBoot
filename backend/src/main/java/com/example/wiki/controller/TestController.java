a08bb988b7d6f06a51dea745c9e2fcb48231c305
lry6789 <cleansexd1@gmail.com>
2023-01-01 16:49:15 -0600

    add hello world api

---
+++ backend/src/main/java/com/example/wiki/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.example.wiki.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    /*
+    * GET POST PUT DELETE
+    * RequestMapping: available for all the kinds of request above
+    * GetMapping: only available for Get
+    * PostMapping
+    * PutMapping
+    * DeleteMapping
+    *
+    * @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    *
+    * /user/1
+    * */
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
