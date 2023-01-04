3043966069c060bc8da8d80c8f8221db9478cf78
lry6789 <cleansexd1@gmail.com>
2023-01-04 17:25:47 -0600

    package respond parameter and request parameter

--- backend/src/main/java/com/example/wiki/controller/EbookController.java
+++ backend/src/main/java/com/example/wiki/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.example.wiki.controller;
 
-import com.example.wiki.domain.Ebook;
+
+import com.example.wiki.req.EbookReq;
 import com.example.wiki.resp.CommonResp;
+import com.example.wiki.resp.EbookResp;
 import com.example.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +20,9 @@ public class EbookController {
     @Resource
     private EbookService ebookService;
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
