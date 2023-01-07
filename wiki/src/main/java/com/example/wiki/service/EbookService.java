b57dfb4a6172060ac6d6f423a7e3caca44b2f2b3
lry6789 <cleansexd1@gmail.com>
2023-01-07 11:48:03 -0600

    add page helper to backend

--- wiki/src/main/java/com/example/wiki/service/EbookService.java
+++ wiki/src/main/java/com/example/wiki/service/EbookService.java
@@ -6,6 +6,10 @@ import com.example.wiki.mapper.EbookMapper;
 import com.example.wiki.req.EbookReq;
 import com.example.wiki.resp.EbookResp;
 import com.example.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -18,15 +22,24 @@ import java.util.List;
 public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
-
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     public List<EbookResp> list(EbookReq req){
+
+
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%"+req.getName()+"%");
         }
 
+        //only work for the next one SQL
+        PageHelper.startPage(1,3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数 {}",pageInfo.getTotal());
+        LOG.info("总页数 {}",pageInfo.getPages());
+
+
 
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook :ebookList) {
