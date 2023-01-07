8f48dc70189f2a0f30955329301ae03c784f2205
lry6789 <cleansexd1@gmail.com>
2023-01-07 17:12:27 -0600

    Integrate validation, add validation for list api

--- wiki/src/main/java/com/example/wiki/req/PageReq.java
+++ wiki/src/main/java/com/example/wiki/req/PageReq.java
@@ -1,5 +1,9 @@
 package com.example.wiki.req;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
 public class PageReq {
 
 
@@ -26,9 +30,11 @@ public class PageReq {
                 ", size=" + size +
                 '}';
     }
-
+    @NotNull(message = "Page number cannot be null")
     private int page;
 
+    @NotNull(message = "Size cannot be null")
+    @Max(value = 1000, message = "Size cannot be larger than 1000")
     private int size;
 
 
