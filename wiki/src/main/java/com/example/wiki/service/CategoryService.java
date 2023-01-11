856953b7c103425d913c569cdc29388d8c023e41
lry6789 <cleansexd1@gmail.com>
2023-01-11 17:11:49 -0600

    Complete Category functionalities including adding, editing, deleting, and searching

---
+++ wiki/src/main/java/com/example/wiki/service/CategoryService.java
@@ -0,0 +1,87 @@
+package com.example.wiki.service;
+
+import com.example.wiki.domain.Category;
+import com.example.wiki.domain.CategoryExample;
+import com.example.wiki.mapper.CategoryMapper;
+import com.example.wiki.req.CategoryQueryReq;
+import com.example.wiki.req.CategorySaveReq;
+import com.example.wiki.resp.CategoryQueryResp;
+import com.example.wiki.resp.PageResp;
+import com.example.wiki.util.CopyUtil;
+import com.example.wiki.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryService.class);
+
+    /*
+    * Query
+    * */
+    public PageResp<CategoryQueryResp> list(CategoryQueryReq req){
+
+
+        CategoryExample categoryExample = new CategoryExample();
+        CategoryExample.Criteria criteria = categoryExample.createCriteria();
+
+
+        //only work for the next one SQL
+        PageHelper.startPage(req.getPage(),req.getSize());
+        List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
+        PageInfo<Category> pageInfo = new PageInfo<>(categoryList);
+        LOG.info("总行数 {}",pageInfo.getTotal());
+        LOG.info("总页数 {}",pageInfo.getPages());
+
+
+
+//        List<CategoryResp> respList = new ArrayList<>();
+//        for (Category category :categoryList) {
+////            CategoryResp categoryResp = new CategoryResp();
+////            BeanUtils.copyProperties(category,categoryResp);
+//             //copy object
+//            CategoryResp categoryResp = CopyUtil.copy(category, CategoryResp.class);
+//            respList.add(categoryResp);
+//
+//        }
+
+         //copy list
+        List<CategoryQueryResp> list = CopyUtil.copyList(categoryList, CategoryQueryResp.class);
+        PageResp<CategoryQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+    /*
+    * Save
+    * */
+    public void save(CategorySaveReq req){
+        Category category = CopyUtil.copy(req, Category.class);
+        if(ObjectUtils.isEmpty(req.getId())){
+            category.setId(snowFlake.nextId());
+            //add new
+            categoryMapper.insert(category);
+        }
+        else{
+            //update
+            categoryMapper.updateByPrimaryKey(category);
+        }
+
+    }
+    public void delete(Long id){
+        categoryMapper.deleteByPrimaryKey(id);
+    }
+}
