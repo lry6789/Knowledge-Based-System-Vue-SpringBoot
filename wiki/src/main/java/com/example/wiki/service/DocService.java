5a468424ac7e659d96221b47d7ffbf7c6ba3e09a
lry6789 <cleansexd1@gmail.com>
2023-01-12 10:13:55 -0600

    Complete functionalities of Doc by copying those of Category

---
+++ wiki/src/main/java/com/example/wiki/service/DocService.java
@@ -0,0 +1,99 @@
+package com.example.wiki.service;
+
+import com.example.wiki.domain.Doc;
+import com.example.wiki.domain.DocExample;
+import com.example.wiki.mapper.DocMapper;
+import com.example.wiki.req.DocQueryReq;
+import com.example.wiki.req.DocSaveReq;
+import com.example.wiki.resp.DocQueryResp;
+import com.example.wiki.resp.PageResp;
+import com.example.wiki.util.CopyUtil;
+import com.example.wiki.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DocService {
+
+    @Resource
+    private DocMapper docMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+    private static final Logger LOG = LoggerFactory.getLogger(DocService.class);
+
+    /*
+    * Query
+    * */
+    public List<DocQueryResp> all(){
+
+
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+         //copy list
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        return list;
+    }    public PageResp<DocQueryResp> list(DocQueryReq req){
+
+
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        DocExample.Criteria criteria = docExample.createCriteria();
+
+
+        //only work for the next one SQL
+        PageHelper.startPage(req.getPage(),req.getSize());
+        List<Doc> docList = docMapper.selectByExample(docExample);
+        PageInfo<Doc> pageInfo = new PageInfo<>(docList);
+        LOG.info("总行数 {}",pageInfo.getTotal());
+        LOG.info("总页数 {}",pageInfo.getPages());
+
+
+
+//        List<DocResp> respList = new ArrayList<>();
+//        for (Doc doc :docList) {
+////            DocResp docResp = new DocResp();
+////            BeanUtils.copyProperties(doc,docResp);
+//             //copy object
+//            DocResp docResp = CopyUtil.copy(doc, DocResp.class);
+//            respList.add(docResp);
+//
+//        }
+
+         //copy list
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+        PageResp<DocQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+    /*
+    * Save
+    * */
+    public void save(DocSaveReq req){
+        Doc doc = CopyUtil.copy(req, Doc.class);
+        if(ObjectUtils.isEmpty(req.getId())){
+            doc.setId(snowFlake.nextId());
+            //add new
+            docMapper.insert(doc);
+        }
+        else{
+            //update
+            docMapper.updateByPrimaryKey(doc);
+        }
+
+    }
+    public void delete(Long id){
+        docMapper.deleteByPrimaryKey(id);
+    }
+}
