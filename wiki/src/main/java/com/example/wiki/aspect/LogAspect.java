6ba5c94adf8c18ab4b8ca5c214b377ae08880aa5
lry6789 <cleansexd1@gmail.com>
2023-01-20 10:35:36 -0600

    the same ip can only vote once per day

--- wiki/src/main/java/com/example/wiki/aspect/LogAspect.java
+++ wiki/src/main/java/com/example/wiki/aspect/LogAspect.java
@@ -16,7 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
+import com.example.wiki.util.RequestContext;
 @Aspect
 @Component
 public class LogAspect {
@@ -42,6 +42,8 @@ public class LogAspect {
         LOG.info("类名方法: {}.{}", signature.getDeclaringTypeName(), name);
         LOG.info("远程地址: {}", request.getRemoteAddr());
 
+
+        RequestContext.setRemoteAddr(getRemoteIp(request));
         // 打印请求参数
         Object[] args = joinPoint.getArgs();
         // LOG.info("请求参数: {}", JSONObject.toJSONString(args));
@@ -75,6 +77,24 @@ public class LogAspect {
         LOG.info("返回结果: {}", JSONObject.toJSONString(result, excludefilter));
         LOG.info("------------- 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
         return result;
+    }
+    /**
+     * 使用nginx做反向代理，需要用该方法才能取到真实的远程IP
+     * @param request
+     * @return
+     */
+    public String getRemoteIp(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
     }
 
 }
--- wiki/src/main/java/com/example/wiki/service/DocService.java
+++ wiki/src/main/java/com/example/wiki/service/DocService.java
@@ -4,6 +4,8 @@ import com.example.wiki.domain.Doc;
 import com.example.wiki.domain.DocExample;
 import com.example.wiki.domain.Content;
 import com.example.wiki.domain.ContentExample;
+import com.example.wiki.exception.BusinessException;
+import com.example.wiki.exception.BusinessExceptionCode;
 import com.example.wiki.mapper.ContentMapper;
 import com.example.wiki.mapper.DocMapper;
 import com.example.wiki.mapper.DocMapperCust;
@@ -12,6 +14,8 @@ import com.example.wiki.req.DocSaveReq;
 import com.example.wiki.resp.DocQueryResp;
 import com.example.wiki.resp.PageResp;
 import com.example.wiki.util.CopyUtil;
+import com.example.wiki.util.RedisUtil;
+import com.example.wiki.util.RequestContext;
 import com.example.wiki.util.SnowFlake;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -32,6 +36,8 @@ public class DocService {
     private DocMapperCust docMapperCust;
     @Resource
     private ContentMapper contentMapper;
+    @Resource
+    public RedisUtil redisUtil;
 
     @Resource
     private SnowFlake snowFlake;
@@ -133,7 +139,14 @@ public class DocService {
      * 点赞
      */
     public void vote(Long id) {
-        docMapperCust.increaseVoteCount(id);
+        // docMapperCust.increaseVoteCount(id);
+        // 远程IP+doc.id作为key，24小时内不能重复
+        String ip = RequestContext.getRemoteAddr();
+        if (redisUtil.validateRepeat("DOC_VOTE_" + id + "_" + ip, 3600 * 24)) {
+            docMapperCust.increaseVoteCount(id);
+        } else {
+            throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
+        }
     }
 
 }
