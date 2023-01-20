84aeb775c8567b733c5a5052e207105306e02c18
lry6789 <cleansexd1@gmail.com>
2023-01-19 21:00:54 -0600

    add login API

--- wiki/src/main/java/com/example/wiki/service/UserService.java
+++ wiki/src/main/java/com/example/wiki/service/UserService.java
@@ -2,7 +2,9 @@ package com.example.wiki.service;
 
 import com.example.wiki.exception.BusinessException;
 import com.example.wiki.exception.BusinessExceptionCode;
+import com.example.wiki.req.UserLoginReq;
 import com.example.wiki.req.UserResetPasswordReq;
+import com.example.wiki.resp.UserLoginResp;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.example.wiki.domain.User;
@@ -96,6 +98,31 @@ public class UserService {
     public void resetPassword(UserResetPasswordReq req) {
         User user = CopyUtil.copy(req, User.class);
         userMapper.updateByPrimaryKeySelective(user);
+    }
+
+    /**
+     *登录
+     */
+
+    public UserLoginResp login(UserLoginReq req) {
+        User userDb = selectByLoginName(req.getLoginName());
+        if(ObjectUtils.isEmpty(userDb)){
+            //用户名不存在
+            LOG.info("用户名不存在, {}", req.getLoginName());
+            throw new BusinessException(BusinessExceptionCode.LOGIN_USER_ERROR);
+        }
+        else{
+            if(userDb.getPassword().equals(req.getLoginName())){
+                //登录成功
+                UserLoginResp userLoginResp = CopyUtil.copy(userDb, UserLoginResp.class);
+                return userLoginResp;
+            }
+            else{
+                //密码错误
+                LOG.info("密码不对, 输入密码：{}, 数据库密码：{}", req.getPassword(), userDb.getPassword());
+                throw new BusinessException(BusinessExceptionCode.LOGIN_USER_ERROR);
+            }
+        }
     }
 
     public void delete(Long id) {
