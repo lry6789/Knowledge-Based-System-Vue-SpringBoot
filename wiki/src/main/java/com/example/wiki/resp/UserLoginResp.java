04ee71f70d8e8d3d8cc927e9b0737954d0eb1d84
lry6789 <cleansexd1@gmail.com>
2023-01-19 21:21:08 -0600

    integrate redis, add token into redis after login successfully

--- wiki/src/main/java/com/example/wiki/resp/UserLoginResp.java
+++ wiki/src/main/java/com/example/wiki/resp/UserLoginResp.java
@@ -12,7 +12,25 @@ public class UserLoginResp {
 
     private String name;
 
+    public String getToken() {
+        return token;
+    }
 
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String toString() {
+        return "UserLoginResp{" +
+                "id=" + id +
+                ", loginName='" + loginName + '\'' +
+                ", name='" + name + '\'' +
+                ", token='" + token + '\'' +
+                '}';
+    }
+
+    private String token;
 
     public Long getId() {
         return id;
@@ -39,17 +57,4 @@ public class UserLoginResp {
     }
 
 
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
-        sb.append("]");
-        return sb.toString();
-    }
 }
