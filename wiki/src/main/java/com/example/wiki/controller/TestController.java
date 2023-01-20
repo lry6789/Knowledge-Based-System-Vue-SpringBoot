87b3da481d17ad337702b1926e2544d26e291e20
lry6789 <cleansexd1@gmail.com>
2023-01-19 21:31:16 -0600

    add testing api for redis

--- wiki/src/main/java/com/example/wiki/controller/TestController.java
+++ wiki/src/main/java/com/example/wiki/controller/TestController.java
@@ -2,44 +2,68 @@ package com.example.wiki.controller;
 
 import com.example.wiki.domain.Test;
 import com.example.wiki.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
-    /*
-    * GET POST PUT DELETE
-    * RequestMapping: available for all the kinds of request above
-    * GetMapping: only available for Get
-    * PostMapping
-    * PutMapping
-    * DeleteMapping
-    *
-    * @RequestMapping(value = "/hello",method = RequestMethod.GET)
-    *
-    * /user/1
-    * */
-    @Value("${test.hello:Hi2}")
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @Value("${test.hello:TEST}")
     private String testHello;
-
-    @RequestMapping("/hello")
-    public String hello(){
-        return "hello world"+testHello;
-    }
-    @PostMapping("/hello/post")
-    public String helloPost(String name){
-        return "hello world! Post."+name ;
-    }
-
 
     @Resource
     private TestService testService;
-    @GetMapping("test/list")
-    public List<Test> list(){
+
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    /**
+     * GET, POST, PUT, DELETE
+     *
+     * /user?id=1
+     * /user/1
+     * @return
+     */
+    // @PostMapping
+    // @PutMapping
+    // @DeleteMapping
+    // @RequestMapping(value = "/user/1", method = RequestMethod.GET)
+    // @RequestMapping(value = "/user/1", method = RequestMethod.DELETE)
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!" + testHello;
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
+    }
+
+    @GetMapping("/test/list")
+    public List<Test> list() {
         return testService.list();
+    }
+
+    @RequestMapping("/redis/set/{key}/{value}")
+    public String set(@PathVariable Long key, @PathVariable String value) {
+        redisTemplate.opsForValue().set(key, value, 3600, TimeUnit.SECONDS);
+        LOG.info("key: {}, value: {}", key, value);
+        return "success";
+    }
+
+    @RequestMapping("/redis/get/{key}")
+    public Object get(@PathVariable Long key) {
+        Object object = redisTemplate.opsForValue().get(key);
+        LOG.info("key: {}, value: {}", key, object);
+        return object;
     }
 }
