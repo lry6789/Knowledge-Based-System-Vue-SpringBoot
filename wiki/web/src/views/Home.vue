53f62989f84843a40299b92baecb455ff25446da
ReeseSun <98066506+WeiruSun@users.noreply.github.com>
2023-01-24 18:35:18 -0600

    5-4 support dynamic SQL

--- wiki/web/src/views/Home.vue
+++ wiki/web/src/views/Home.vue
@@ -2,17 +2,12 @@
   <a-layout>
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
       >
         <a-sub-menu key="sub1">
           <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
+            <span><user-outlined />subnav 11111</span>
           </template>
           <a-menu-item key="1">option1</a-menu-item>
           <a-menu-item key="2">option2</a-menu-item>
@@ -21,10 +16,7 @@
         </a-sub-menu>
         <a-sub-menu key="sub2">
           <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
+            <span><laptop-outlined />subnav 2</span>
           </template>
           <a-menu-item key="5">option5</a-menu-item>
           <a-menu-item key="6">option6</a-menu-item>
@@ -33,10 +25,7 @@
         </a-sub-menu>
         <a-sub-menu key="sub3">
           <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
+            <span><notification-outlined />subnav 3</span>
           </template>
           <a-menu-item key="9">option9</a-menu-item>
           <a-menu-item key="10">option10</a-menu-item>
@@ -48,58 +37,38 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-<!--      <pre>-->
-<!--        {{ebooks}}-->
-<!--      {{ebooks2}}</pre>-->
-
-      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-        <template #footer>
-          <div>
-            <b>ant design vue</b>
-            footer part
-          </div>
-        </template>
+      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
         <template #renderItem="{ item }">
-          <a-list-item key="item.title">
+          <a-list-item key="item.name">
             <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component :is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
-            </template>
-            <template #extra>
-              <img
-                  width="272"
-                  alt="logo"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-              />
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
             </template>
             <a-list-item-meta :description="item.description">
               <template #title>
-                <a :href="item.href">{{ item.title }}</a>
+                <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #avatar><a-avatar :src="item.avatar" /></template>
+              <template #avatar><a-avatar :src="item.cover"/></template>
             </a-list-item-meta>
-            {{ item.content }}
           </a-list-item>
         </template>
       </a-list>
-
     </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
-import axios from "axios";
-import {defineComponent,onMounted,ref,reactive,toRef} from "vue";
+import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
+import axios from 'axios';
 
 const listData: any = [];
-
 for (let i = 0; i < 23; i++) {
   listData.push({
     href: 'https://www.antdv.com/',
     title: `ant design vue part ${i}`,
-    avatar: 'https://joeschmoe.io/api/v1/random',
+    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
     description:
         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
     content:
@@ -109,43 +78,47 @@ for (let i = 0; i < 23; i++) {
 
 export default defineComponent({
   name: 'Home',
-  setup(){
+  setup() {
     console.log("setup");
-    //响应式数据 ref(
     const ebooks = ref();
-    const ebooks1 = reactive({books:[]});
+    const ebooks1 = reactive({books: []});
 
-    const pagination = {
-      onChange: (page: any) => {
-        console.log(page);
-      },
-      pageSize: 3,
-    };
-    const actions: Record<string, string>[] = [
-      { type: 'StarOutlined', text: '156' },
-      { type: 'LikeOutlined', text: '156' },
-      { type: 'MessageOutlined', text: '2' },
-    ];
-
-
-    onMounted(()=>{
-        console.log("onMounted");
-        axios.get(" http://127.0.0.1:8080/ebook/list?name=Spring").then((response) =>{
-          const data = response.data;
-          ebooks.value = data.content;
-          ebooks1.books = data.content;
-          console.log(response);
+    onMounted(() => {
+      console.log("onMounted");
+      axios.get("http://127.0.0.1:8080/ebook/list").then((response) => {
+        const data = response.data;
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
+        console.log(response);
       });
-    })
+    });
 
     return {
       ebooks,
-      ebooks2:toRef(ebooks1,"books"),
+      ebooks2: toRef(ebooks1, "books"),
       listData,
-      pagination,
-      actions,
-
+      pagination: {
+        onChange: (page: any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        { type: 'StarOutlined', text: '156' },
+        { type: 'LikeOutlined', text: '156' },
+        { type: 'MessageOutlined', text: '2' },
+      ],
     }
   }
 });
 </script>
+
+<style scoped>
+.ant-avatar {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
+</style>
