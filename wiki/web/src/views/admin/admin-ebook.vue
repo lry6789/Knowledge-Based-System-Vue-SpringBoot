d7e69d01513138fa3a588fe0d2093ae92fa2d985
ReeseSun <98066506+WeiruSun@users.noreply.github.com>
2023-01-26 23:49:47 -0600

    6-7 Modal Dialogue Box

--- wiki/web/src/views/admin/admin-ebook.vue
+++ wiki/web/src/views/admin/admin-ebook.vue
@@ -16,7 +16,7 @@
         </template>
         <template v-slot:action="{ text, record }">
           <a-space size="small">
-            <a-button type="primary">
+            <a-button type="primary" @click="edit(record)">
               编辑
             </a-button>
             <a-button type="danger">
@@ -27,6 +27,31 @@
       </a-table>
     </a-layout-content>
   </a-layout>
+
+  <a-modal
+      title="电子书表单"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
+  >
+    <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover" />
+      </a-form-item>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name" />
+      </a-form-item>
+      <a-form-item label="分类一">
+        <a-input v-model:value="ebook.category1Id" />
+      </a-form-item>
+      <a-form-item label="分类二">
+        <a-input v-model:value="ebook.category2Id" />
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.desc" type="textarea" />
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
 
 <script lang="ts">
@@ -87,13 +112,12 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
-      //axios.get("http://127.0.0.1:8080/ebook/list", params).
-      axios.get("http://127.0.0.1:8080/ebook/list", {
+      axios.get("/ebook/list", {
         params: {
           page: params.page,
           size: params.size
-        }}).
-      then((response) => {
+        }
+      }).then((response) => {
         loading.value = false;
         const data = response.data;
         ebooks.value = data.content.list;
@@ -115,11 +139,31 @@ export default defineComponent({
       });
     };
 
+    // -------- 表单 ---------
+    const ebook = ref({});
+    const modalVisible = ref(false);
+    const modalLoading = ref(false);
+    const handleModalOk = () => {
+      modalLoading.value = true;
+      setTimeout(() => {
+        modalVisible.value = false;
+        modalLoading.value = false;
+      }, 2000);
+    };
+
+    /**
+     * 编辑
+     */
+    const edit = (record: any) => {
+      modalVisible.value = true;
+      ebook.value = record
+    };
+
     onMounted(() => {
       handleQuery({
-                    page: 1,
-                    size: pagination.value.pageSize,
-                  });
+        page: 1,
+        size: pagination.value.pageSize,
+      });
     });
 
     return {
@@ -127,7 +171,14 @@ export default defineComponent({
       pagination,
       columns,
       loading,
-      handleTableChange
+      handleTableChange,
+
+      edit,
+
+      ebook,
+      modalVisible,
+      modalLoading,
+      handleModalOk
     }
   }
 });
