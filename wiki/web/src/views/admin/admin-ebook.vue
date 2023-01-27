b1b86cd5b243d2858035f38bc4382429e8258873
ReeseSun <98066506+WeiruSun@users.noreply.github.com>
2023-01-27 00:13:22 -0600

    6-7 integrate snowflake with front-end

--- wiki/web/src/views/admin/admin-ebook.vue
+++ wiki/web/src/views/admin/admin-ebook.vue
@@ -3,6 +3,11 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
+      <p>
+        <a-button type="primary" @click="add()" size="large">
+          新增
+          </a-button>
+      </p>
       <a-table
           :columns="columns"
           :row-key="record => record.id"
@@ -168,6 +173,12 @@ export default defineComponent({
       ebook.value = record
     };
 
+    /**add**/
+    const add =()=>{
+      modalVisible.value = true;
+      ebook.value = {};
+    }
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -183,6 +194,7 @@ export default defineComponent({
       handleTableChange,
 
       edit,
+      add,
 
       ebook,
       modalVisible,
