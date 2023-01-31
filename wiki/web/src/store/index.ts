43c2a5502f73c3bf46971475a9204abdeffd65a9
ReeseSun <98066506+WeiruSun@users.noreply.github.com>
2023-01-30 22:40:08 -0600

    9-9 use vuex to integrate sessionStorage

--- wiki/web/src/store/index.ts
+++ wiki/web/src/store/index.ts
@@ -1,18 +1,20 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
+
+declare let SessionStorage: any;
+const USER = "USER";
 
 const store = createStore({
-  state: {
-    user: {}
-  },
-  mutations: {
-    setUser (state, user) {
-      state.user = user;
-    }
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        user: SessionStorage.get(USER) || {}
+    },
+    mutations: {
+        setUser(state, user) {
+            state.user = user;
+            SessionStorage.set(USER, user);
+        }
+    },
+    actions: {},
+    modules: {}
 });
 
 export default store;
