3c642385a988b91c729b841eaf2b9183c7debdc5
ReeseSun <98066506+WeiruSun@users.noreply.github.com>
2023-01-30 22:19:20 -0600

    9-3

--- wiki/web/src/components/the-header.vue
+++ wiki/web/src/components/the-header.vue
@@ -2,22 +2,24 @@
   <a-layout-header class="header">
     <div class="logo" />
     <a-menu
-        v-model:selectedKeys="selectedKeys1"
         theme="dark"
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
       <a-menu-item key="/">
-        <router-link to="/">Home</router-link>
+        <router-link to="/">首页</router-link>
       </a-menu-item>
-      <a-menu-item key="/ebook">
-        <router-link to="/ebook">Ebook</router-link>
+      <a-menu-item key="/admin/user">
+        <router-link to="/admin/user">用户管理</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook">
+        <router-link to="/admin/ebook">电子书管理</router-link>
       </a-menu-item>
       <a-menu-item key="/admin/category">
-        <router-link to="/admin/category">Classification management</router-link>
+        <router-link to="/admin/category">分类管理</router-link>
       </a-menu-item>
       <a-menu-item key="/about">
-        <router-link to="/about">About</router-link>
+        <router-link to="/about">关于我们</router-link>
       </a-menu-item>
     </a-menu>
   </a-layout-header>
@@ -27,7 +29,6 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'the-header',
-
+  name: 'the-header'
 });
 </script>
--- wiki/web/src/router/index.ts
+++ wiki/web/src/router/index.ts
@@ -1,10 +1,11 @@
-import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
-import About from "../views/About.vue";
-import adminEbook from "@/views/admin/admin-ebook.vue";
-import adminCategory from "@/views/admin/admin-category.vue";
-import AdminDoc from '../views/admin/admin-doc.vue'
+import About from '../views/About.vue'
 import Doc from '../views/doc.vue'
+import AdminUser from '../views/admin/admin-user.vue'
+import AdminEbook from '../views/admin/admin-ebook.vue'
+import AdminCategory from '../views/admin/admin-category.vue'
+import AdminDoc from '../views/admin/admin-doc.vue'
 
 const routes: Array<RouteRecordRaw> = [
     {
@@ -24,16 +25,22 @@ const routes: Array<RouteRecordRaw> = [
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
+        // component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
     },
     {
-        path: '/ebook',
-        name: 'admin-ebook',
-        component: adminEbook
+        path: '/admin/user',
+        name: 'AdminUser',
+        component: AdminUser
+    },
+    {
+        path: '/admin/ebook',
+        name: 'AdminEbook',
+        component: AdminEbook
     },
     {
         path: '/admin/category',
         name: 'AdminCategory',
-        component: adminCategory
+        component: AdminCategory
     },
     {
         path: '/admin/doc',
